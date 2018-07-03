a3bab778accf2332abedcfc84b23eabcac78a4f6
Samuel Soares <samuel_soares27@hotmail.com>
2018-07-03 19:43:11 -0300

    Classe usuário com get e set já criados

--- Usuario.php
+++ Usuario.php
@@ -1,7 +1,39 @@
 <?php
 
 class Usuario{
-	
+	//Classe usuário com Get e Set
+	private $idusuario;
+	public function getIdusuario(){
+		return $this->idusuario;
+	}
+	public function setIdusuario($value){
+		$this->idusuario = $value;
+	}
+
+	private $deslogin;
+	public function getDeslogin(){
+		return $this->deslogin;
+	}
+	public function setDeslogin($value){
+		$this->deslogin = $value;
+	}
+
+	private $dessenha;
+	public function getDessenha(){
+		return $this->dessenha;
+	}
+
+	public function setDessenha($value){
+		$this->dessenha = $value;
+	}
+
+	private $dtcadastro;
+	public function getDtcadastro(){
+		return $this->getDescadastro;
+	}
+	public function setDtcadastro($value){
+		$this->dtcadastro = $value;
+	}
 }
 
 ?>
